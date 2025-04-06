54e48382678b0c004e78c9fbde1018195a3702c6
Kruno Tomola Fabro <krunotf@gmail.com>
2025-04-06 16:22:38 +0200

    Use AxumBody

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,11 @@ use pin_project::pin_project;
 use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::{future::Future, pin::Pin, task::ready};
+use tonic::service::AxumBody;
 use tower::{Layer, Service};
 
 pub type Error = tonic::Status;
-pub type Body = tonic::body::Body;
+pub type Body = AxumBody;
 
 /// The Layer type that implements tower::Layer and is passed into `.layer()`
 pub struct GovernorLayer<K, M>
